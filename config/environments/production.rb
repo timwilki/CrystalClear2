75fe433402cbe04372bef489f644551c23127e23
tsw@tswdesign.co.uk <tsw@tswdesign.co.uk>
2025-08-18 09:50:33 +0100

    added optional wildcard

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,8 +97,9 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # Allow Render hostnames
-  config.hosts << "https://crystalclear.onrender.com"
+  config.hosts << "crystalclear.onrender.com"
   config.hosts << /.*\.onrender\.com/
+  config.hosts << ENV["RAILS_ALLOWED_HOST"] if ENV["RAILS_ALLOWED_HOST"].present?
 
   # Serve static files when the platform exposes them via env var
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
