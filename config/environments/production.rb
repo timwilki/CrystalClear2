665b3e5dded6e25f2ad17215b4be737eb095f521
tsw@tswdesign.co.uk <tsw@tswdesign.co.uk>
2025-08-18 10:17:42 +0100

    hosts nil

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # Allow Render hostnames
-config.hosts.clear
+config.hosts = nil
 
   # Serve static files when the platform exposes them via env var
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
